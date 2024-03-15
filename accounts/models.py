598dd4a036f4a44a856e12a4d7988e1b805d91ff
rishulayerfive <61156797+rishulayerfive@users.noreply.github.com>
2024-03-15 12:24:56 +0530

    Fix

--- accounts/models.py
+++ accounts/models.py
@@ -18,7 +18,7 @@ class UserManager(BaseUserManager):
         return user
 
     def create_user(self, username=None, email=None, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(username, email, password, **extra_fields)
 
