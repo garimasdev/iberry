14e88c400f907275b173fc2ad27fefce5ecb27b8
garima-l5 <garima.sachdeva@layerfive.com>
2024-05-10 19:22:53 +0530

    test phonpe

--- stores/views.py
+++ stores/views.py
@@ -388,7 +388,7 @@ from phonepe.sdk.pg.payments.v1.models.request.pg_pay_request import PgPayReques
 def CreatePaymentOrder(request):
     if request.method == 'POST':
         try:
-            phonepe_client = PhonePePaymentClient(merchant_id=settings.MERCHANT_ID, salt_key=settings.SECRET_KEY, salt_index=1, env=settings.ENV)
+            phonepe_client = PhonePePaymentClient(merchant_id=settings.MERCHANT_ID, salt_key=settings.SALT_KEY, salt_index=1, env=settings.ENV)
             # phonepe_client = PhonePePaymentClient(merchant_id=settings.MERCHANT_ID, salt_key=settings.SECRET_KEY, salt_index=settings.SALT_INDEX, env=settings.ENV, timezone=pytz.timezone(settings.TIME_ZONE))
             unique_transaction_id = str(uuid.uuid4())[:-2]
             ui_redirect_url = f"{request.scheme}://{request.get_host()}/payment/checkout/success"  
