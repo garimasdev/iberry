4930cf50f5567c9e7d9fdecd0cb4e5fe1900da5b
garima-l5 <garima.sachdeva@layerfive.com>
2024-06-07 02:12:19 +0530

    changes in service

--- core/service-worker.js
+++ core/service-worker.js
@@ -15,36 +15,42 @@
 
 
 
+self.addEventListener('fetch', function(event) {});
 
-var staticCacheName = 'djangopwa-v1';
 
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll([
-        '/', // Cache the root URL
-        '/offline.html', // Cache an offline page
-        '/css/styles.css', // Cache CSS files
-        '/js/script.js', // Cache JavaScript files
-      ]);
-    })
-  );
-});
 
-self.addEventListener('fetch', function (event) {
-  var requestUrl = new URL(event.request.url);
-  if (requestUrl.origin === location.origin) {
-    if ((requestUrl.pathname === '/')) {
-      event.respondWith(caches.match('/offline.html')); // Respond with offline page if root URL is requested
-      return;
-    }
-  }
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request); // Try to fetch from cache, fallback to network
-    })
-  );
-});
+
+
+
+// var staticCacheName = 'djangopwa-v1';
+
+// self.addEventListener('install', function (event) {
+//   event.waitUntil(
+//     caches.open(staticCacheName).then(function (cache) {
+//       return cache.addAll([
+//         '/', // Cache the root URL
+//         '/offline.html', // Cache an offline page
+//         '/css/styles.css', // Cache CSS files
+//         '/js/script.js', // Cache JavaScript files
+//       ]);
+//     })
+//   );
+// });
+
+// self.addEventListener('fetch', function (event) {
+//   var requestUrl = new URL(event.request.url);
+//   if (requestUrl.origin === location.origin) {
+//     if ((requestUrl.pathname === '/')) {
+//       event.respondWith(caches.match('/offline.html')); // Respond with offline page if root URL is requested
+//       return;
+//     }
+//   }
+//   event.respondWith(
+//     caches.match(event.request).then(function (response) {
+//       return response || fetch(event.request); // Try to fetch from cache, fallback to network
+//     })
+//   );
+// });
 
 
 
