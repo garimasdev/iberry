e04aa82056e7bb9b9d0f9da39cff2cce43f1a113
garima-l5 <garima.sachdeva@layerfive.com>
2024-09-04 16:11:39 +0530

    push notifcation,outdoor comments &  title

--- static/js/firebase-messaging-sw.js
+++ static/js/firebase-messaging-sw.js
@@ -4,41 +4,41 @@ importScripts("https://www.gstatic.com/firebasejs/8.6.3/firebase-messaging.js");
 
 // Initialize the  app in the service worker by passing the generated config
 const firebaseConfig = {
-  apiKey: "AIzaSyD1vQc_WZn5rWpnKXPeLRROeTTm85eDAOk",
-  authDomain: "iberry-eeb59.firebaseapp.com",
-  projectId: "iberry-eeb59",
-  storageBucket: "iberry-eeb59.appspot.com",
-  messagingSenderId: "498314626184",
-  appId: "1:498314626184:web:69dc20992e8652da9bcb3d",
-  measurementId: "G-ER8SL9T0YM"
+  apiKey: "AIzaSyB3YnNXnBSDkuJp3QZKYYgnM52Jwawipoc",
+  authDomain: "iberry-81920.firebaseapp.com",
+  projectId: "iberry-81920",
+  storageBucket: "iberry-81920.appspot.com",
+  messagingSenderId: "661156171796",
+  appId: "1:661156171796:web:eabde955604c4b64bd9701",
+  measurementId: "G-9M4YJQ5DLL"
 };
 
-function speak(text) {
-  const utterThis = self.SpeechSynthesisUtterance(text);
+// function speak(text) {
+//   const utterThis = self.SpeechSynthesisUtterance(text);
 
-  utterThis.onend = function (event) {
-    console.log("SpeechSynthesisUtterance.onend");
-  };
+//   utterThis.onend = function (event) {
+//     console.log("SpeechSynthesisUtterance.onend");
+//   };
 
-  utterThis.onerror = function (event) {
-    console.log("SpeechSynthesisUtterance.onerror");
-  };
+//   utterThis.onerror = function (event) {
+//     console.log("SpeechSynthesisUtterance.onerror");
+//   };
 
-  // const selectedOption = voiceSelect.selectedOptions[0].getAttribute("data-name");
+//   // const selectedOption = voiceSelect.selectedOptions[0].getAttribute("data-name");
 
-  // for (let i = 0; i < voices.length; i++) {
-  //   if (voices[i].name === selectedOption) {
-  //     utterThis.voice = voices[i];
-  //     break;
-  //   }
-  // }
-  // console.log("The voice name", pitch.value)
-  // console.log("The voice name", rate.value)
-  utterThis.pitch = 1;
-  utterThis.rate = 1;
-  speechSynthesis.speak(utterThis);
-  // synth.speak(utterThis);
-}
+//   // for (let i = 0; i < voices.length; i++) {
+//   //   if (voices[i].name === selectedOption) {
+//   //     utterThis.voice = voices[i];
+//   //     break;
+//   //   }
+//   // }
+//   // console.log("The voice name", pitch.value)
+//   // console.log("The voice name", rate.value)
+//   utterThis.pitch = 1;
+//   utterThis.rate = 1;
+//   // speechSynthesis.speak(utterThis);
+//   // synth.speak(utterThis);
+// }
 
 firebase.initializeApp(firebaseConfig); 
 const messaging=firebase.messaging(); 
@@ -48,14 +48,14 @@ console.log('Received background message ', payload);
 // Check if the browser supports the SpeechSynthesis API
 const notificationTitle = payload.notification.title;
 console.log("Notification title", notificationTitle)
-const audio=new Audio("/static/iphone_sound.mp3"); 
-audio.play(); 
+// const audio=new Audio("/static/iphone_sound.mp3"); 
+// audio.play(); 
 const notificationOptions = {
     body: payload.notification.body,
 };
 
 self.registration.showNotification(notificationTitle, notificationOptions);
-speak(notificationTitle);
+// speak(notificationTitle);
 });
 
 
