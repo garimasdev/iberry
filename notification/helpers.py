402283c7bdd2c5498732f20e0486bdd9b2514afd
garima-l5 <garima.sachdeva@layerfive.com>
2024-09-07 00:25:57 +0530

    fix creds

--- notification/helpers.py
+++ notification/helpers.py
@@ -6,13 +6,28 @@ import google.auth
 from google.oauth2 import service_account
 import google.auth.transport.requests
 import requests
+from decouple  import config
+
 
 
 # generating a new access token in firebase
 def firebase_access_token():
-    SERVICE_ACCOUNT_FILE = '/Users/garimasachdeva/Desktop/iberry/iberry/stores/credentials.json'
+    creds = {
+        "type": config("type", cast=str),
+        "project_id": config("project_id",  cast=str),
+        "private_key_id": config("private_key_id",  cast=str),
+        "private_key": config("private_key",  cast=str),
+        "client_email": config("client_email",  cast=str),
+        "client_id": config("client_id",  cast=str),
+        "auth_uri": config("auth_uri",  cast=str),
+        "token_uri": config("token_uri",  cast=str),
+        "auth_provider_x509_cert_url": config("auth_provider_x509_cert_url",  cast=str),
+        "client_x509_cert_url": config("client_x509_cert_url",  cast=str),
+        "universe_domain": config("universe_domain",  cast=str),
+    }
+    # SERVICE_ACCOUNT_FILE = '/Users/garimasachdeva/Desktop/iberry/iberry/stores/credentials.json'
     SCOPES = ['https://www.googleapis.com/auth/firebase.messaging']
-    credentials = service_account.Credentials.from_service_account_file( SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    credentials = service_account.Credentials.from_service_account_file( creds, scopes=SCOPES)
     request = google.auth.transport.requests.Request()
     credentials.refresh(request)
     access_token = credentials.token
@@ -24,7 +39,7 @@ def firebase_access_token():
 # sending a push notification through firebases
 def push_notification(body, title, token):
     url = 'https://fcm.googleapis.com/v1/projects/iberry-81920/messages:send'
-    access_token = 'ya29.c.c0ASRK0GbFRLGdNt-9W6YJjvXJQKjzysxcrMBvasK8o5cOZJEEbHtt2jU8fx7OdjfzdRolftWUJKaQ88-EVvVjvkjlz6N1iQg9-gi4_lgWSFOyTWCp0e2ApoR0MO3cbvB9aQrzr_7KpeVWwwc0RqYjNBWFKgKFSO7ApxbvsNAC7opvpZCMtnn7Wbw-Mo_mPBrWKXkTHBRrHTZC-wenkemxBbkOQibIOE2rN4wmH_WrRra9NDQ3Fw1yQPneocj-tR-Mu_5KsFc2AAR8F7815RCwm5HBS2GCAep4FPWbi5r2NBxF8acAJVvNrYSNwbWjeSFsfBUy5zhOZQ_mvAA3NOIrKyEpKrKVJzlzXga7nGkxUIAJejK-G_nzhAYE384KZQ2c5zoFxds_VMZ34absV7wlOFnwgMq_fiOr19jo8fherhn8IvfkeMqz296zuVYS7qq8QVytheibzMY8wUQFvwQa6zwg5me9nnyJF5XYIFpt1Wzu9ZhRetfYum9Yx6nMBuuZkMtr9rWm1zb45jlhMZ6w62gBs-BR_Bq7lFgMfqnIM90o-u7StSIJFk13zdhIyxzgZ6Rgxdv4VFQaWjQFaleov0Oap1g1oqcb3uSs6htJMbs_36ySc7M5hmnQa-zz7lZRW5-ysh5JkWrjfg98Q73Zx7cZoXwk2B44q6iutnlvRcu__Y8nWzz69e0XMpys_sn_iUF6p41tke6b_B5FkfhI03Z-Fej9Wq92pj8bvVFoimmBs2fhekBZmSZ6z-r9X0dvZ57w43gR32n96Rs2ld55fhR9Yc1S4V47O9FuyJugkrkV_bpefMp6wf5pd6QJMWZ4J6fyMXRRpyZv19l8vUm-tbI9d-ISozpvUmro0wlIjhSF6zWd5tXb3_5hjlSqfOy3s8Ixdbk-vBQzRlsXRMxtq1sdbifBMUnzfdBWubiXkyfFnWza7n2bi_6d1StyF1R6_ttf2z7MRVxRkmaccoB-5bSfomSUtSkolxhnvUXu5xl20atvZht08Rj'
+    access_token = 'ya29.c.c0ASRK0Gbdgd2Nd87YmKFazIwtwv4bkxZEyd2K7wK0oYfuMYMgCy33lPCU7mraioZWN7M7rlJtSopxt4Oi7gYkyaFOJnkKhb1ehAjpqp0dUIeYCWunNukX_NBkh0ncMAftXDJueFhPU9LAgFaMVz9tWRmQBm_B7gTKExpVX4DTCQD-rGVP7nQKJbP_0dH1J-nBOpc1vKdX1U-AwLsmmZscpO6GWAIZdXpFpUG7vpVxj8NUfxjUGZtizgqDiQvJs1iGPhZfPdZ9QTVftkyOoT5DxSiirPREWLpI6B6iZ707E73yQkuaWsDV7Vlc9nKw4_iape1Yr12P3XiQAyMnqR_tET5xEojNMVxICbwN65YhaMbv8wqAULHKLtwN384A2lOWrsnS0rMgoWZM615IQiFZoe5fa01Y6ux4SwWj3th3xVle75snYsdJ79Oozik5r61-0xqhdwp0pXvrcoBU9e2awj8uosp9Mcewr73zlxU-kc7hdgQn-RFq52xgdRcX1X6wkwucucmheJzS8tJua1Vpv5VFzshYybZf4Oki9bxSWpll_k2lwM5ww1t96mmliXryfQgoMgZS5mrXOWsFbJhcmnqu8lvhpi4WdVsgbtV8Xv5o1Mgis-O5jB1lUX4_Jj0nS2OOFrasBfkkBzoSo08_ymRocO9vyya_-kee1smOcBFbQqM3QJUk7MXIRe24xu1da6-ReZYthY3pqp0S250qk1nXZIqn6s3o2xZsms1hhuRMWJdsQepVwlc-7yvx6OQ_5jVM93tSJ96-clQYezo1-lVn8lBoVt07kuBJeW2ohmeRxJt92QyM8Z9sQIw3k-opS2OwO7qQf0f4gMU6FdrohVY1XVOQ0SZcjYIUOSUJ_b4ofUwZtwQk-4iUzMIkFVuxkuvqk43OpgaxXXVbu4ppIFUZleR_iVf9a0SgF02457njusxBtWQakaRbon1c2W4t68RUrXmj6vpZOIS40Js3yoBeq3yB_-vOfIuZ73ucoBwJImq39UgRv29'
     headers  =  {
         'Authorization': f'Bearer {access_token}',
         'Content-Type': 'application/json'
